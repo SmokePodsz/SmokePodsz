f7e603361fca018728c5367cdb5ef6d9a657d942
SmokePodsz <teckpods1@gmail.com>
2024-07-01 12:41:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-function toggleMenu() {
-    const menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('hidden');
-}
+// Adicione isto ao seu arquivo home.js
 
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
