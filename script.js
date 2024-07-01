a421265468021c1bc95966d6cb509de08b1cc156
SmokePodsz <teckpods1@gmail.com>
2024-07-01 12:46:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
         navMenu.classList.toggle('show');
     });
 });
+
+function toggleMenu() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+}
