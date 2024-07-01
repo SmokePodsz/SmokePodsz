1e83879187808f19018c7664dfef0f94c8e86e2d
SmokePodsz <teckpods1@gmail.com>
2024-07-01 13:52:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// Adicione isto ao seu arquivo home.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('nav ul');
-
-    menuToggle.addEventListener('click', function() {
-        navMenu.classList.toggle('show');
-    });
-});
-
 function toggleMenu() {
     const menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('hidden');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
 }
