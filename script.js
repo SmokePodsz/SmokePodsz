164c8118ee81d5dd0767af433eab590367177e57
SmokePodsz <teckpods1@gmail.com>
2024-07-02 00:44:10 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById('mobile-menu');
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-}
